1d2f54fdc308aad5d7966b27e02dddf855dcc581
Sharon <srcbun@gmail.com>
2019-02-19 12:43:58 -0500

    Update main.py

--- main.py
+++ main.py
@@ -4,38 +4,38 @@
 # contains at least one uppercase letter
 # contains at least one special character
 
-def minimumNumber(n, password):
+def minimum_number(n, password):
     # determines the strength of a given password
     password = list(password)
-    isStrong = 0
-    charsNeeded = 0
-    hasLower = False
-    hasUpper = False
-    hasDigit = False
-    hasSpecial = False
+    is_strong = 0
+    chars_needed = 0
+    has_lower = False
+    has_upper = False
+    has_digit = False
+    has_special = False
     for i in range(0, n):
-        if(password[i].isdigit() and not hasDigit):
-            hasDigit = True
-            isStrong += 1
-        if(password[i].islower() and not hasLower):
-            hasLower = True
-            isStrong = 1
-        if(password[i].isupper() and not hasUpper):
-            hasUpper = True
-            isStrong += 1
-        if(password[i] in "!@#$%^&*()-+" and not hasSpecial):
-            hasSpecial = True
-            isStrong += 1
+        if(password[i].isdigit() and not has_digit):
+            has_digit = True
+            is_strong += 1
+        if(password[i].islower() and not has_lower):
+            has_lower = True
+            is_strong = 1
+        if(password[i].isupper() and not has_upper):
+            has_upper = True
+            is_strong += 1
+        if(password[i] in "!@#$%^&*()-+" and not has_special):
+            has_special = True
+            is_strong += 1
             
-    charsNeeded = max(6-n, 4-isStrong)
-    if(charsNeeded == 0):
+    chars_needed = max(6-n, 4-is_strong)
+    if(chars_needed == 0):
         print("Strong")
-    elif(charsNeeded < 3):
+    elif(chars_needed < 3):
         print("Medium")
     else:
         print("Weak")
 
 password = input()
 n = len(password)
-check = minimumNumber(n, password)
+check = minimum_number(n, password)
 print(check)
